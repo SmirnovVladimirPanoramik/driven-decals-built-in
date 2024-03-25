9d956a89310e85a4861aa6d53802314ccd08d94f
smirnov.vladimir <smirnov.vladimir@panoramikltd.com>
2024-03-25 15:38:07 +0300

    recreate decal assets and remove fatal exception

--- Editor/DecalMenuItem.cs
+++ Editor/DecalMenuItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
@@ -16,9 +17,9 @@ namespace SamDriver.Decal
 
             // do NOT set to be used in static batching - Unity breaks something with UVs
             GameObjectUtility.SetStaticEditorFlags(decalObject,
-              StaticEditorFlags.ContributeGI |
-              StaticEditorFlags.OccludeeStatic |
-              StaticEditorFlags.ReflectionProbeStatic
+                StaticEditorFlags.ContributeGI |
+                StaticEditorFlags.OccludeeStatic |
+                StaticEditorFlags.ReflectionProbeStatic
             );
 
             // decals shouldn't cast shadows
@@ -30,21 +31,28 @@ namespace SamDriver.Decal
                 decalObject.transform.SetParent(Selection.activeTransform, false);
             }
 
-            decal.DecalAsset = FetchSampleDecalAsset();
-
             // generate initial quad mesh
             decal.GenerateSimpleQuadMesh();
+
+            decal.DecalAsset = FetchSampleDecalAsset();
 
             // select the created item (mimicking behaviour of adding a built-in object)
             Selection.activeObject = decalObject;
         }
 
         const string sampleDecalsPath = "Packages/com.samdriver.driven-decals/Runtime/Sample Decals/";
+
         static DecalAsset FetchSampleDecalAsset()
         {
+            if (!Directory.Exists(sampleDecalsPath))
+            {
+                Debug.LogError($"Couldn't find Directory {sampleDecalsPath}");
+                return null;
+            }
+
             var path = Path.Combine(sampleDecalsPath, "Simple/Grid.asset");
             var decal = AssetDatabase.LoadAssetAtPath<DecalAsset>(path);
-            if (decal == null) throw new FileNotFoundException($"Couldn't find Decal Asset at {path}");
+            if (decal == null) Debug.LogError($"Couldn't find Decal Asset at {path}");
             return decal;
         }
     }
